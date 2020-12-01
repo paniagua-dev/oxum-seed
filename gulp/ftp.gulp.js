67c8e51df9ff9288a6e42c606d80f1a25a153885
Julio Paniagua <julio.paniagua@factor8.ch>
2020-12-01 19:30:54 +0100

    Update configuration of the ftp gulp task

--- gulp/ftp.gulp.js
+++ gulp/ftp.gulp.js
@@ -1,3 +1,9 @@
+/**
+ * This is a gulp task that allow to transfer files to a remote server.
+ * Should be run in a npm script. eg.:  "npm run gulp --ftp={HOST} --ftp-user={USER} --ftp-pwd={PWD}"
+ * The npm command can has as argument --ftp={HOST} --ftp-user={USER} --ftp-pwd={PWD} --ftp-remote-path={REMOTEPATH}
+ * @type {FtpConnection}
+ */
 const ftp = require('vinyl-ftp');
 const {src} = require('gulp');
 const packageJson = require('../package.json');
@@ -7,6 +13,7 @@ const FTPS = function () {
         host: process.env.npm_config_ftp,
         user: process.env.npm_config_ftp_user,
         password: process.env.npm_config_ftp_pwd,
+        remotePath: process.env.npm_config_ftp_remote_path || packageJson.ftp.remotePath || '/paniagua-theme',
         secure: true,
         reload: true,
         log: (...args) => {
@@ -16,9 +23,9 @@ const FTPS = function () {
 
     const connection = ftp.create(credentials);
 
-    return src('./build/**/*', {base: '.', buffer: true})
-        .pipe(connection.newer(packageJson.ftp.remotePath))
-        .pipe(connection.dest(packageJson.ftp.remotePath));
+    return src('./build/**/*', {base: './build', buffer: true})
+        .pipe(connection.newer(credentials.remotePath))
+        .pipe(connection.dest(credentials.remotePath));
 
 };
 
