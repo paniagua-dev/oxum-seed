5fdd09e877e578c0ed23823e82fef1f81e029935
julio <julio@paniagua.dev>
2021-01-07 16:44:48 +0100

    Fix method print_processed_html

--- src/server/functions.php
+++ src/server/functions.php
@@ -1,5 +1,5 @@
 <?php
-require_once ('class/oxum.class.php');
+require_once('class/oxum.class.php');
 $oxum = new WP_Oxum();
 $oxum->preset_wordpress();
 function enqueue_scripts() {
@@ -8,20 +8,18 @@ function enqueue_scripts() {
     $oxum->enqueue_style('js/artwork-ui/artwork-ui.css');
     $oxum->enqueue_style('js/artwork-ui/artwork-ui.core.css');
     //@TODO: Find a way to include artwork-ui.js in the UI build
-    $oxum->enqueue_script('artwork-ui.js','js/artwork-ui/');
+    $oxum->enqueue_script('artwork-ui.js', 'js/artwork-ui/');
     $oxum->enqueue_script('artwork-ui.core.js', 'js/artwork-ui/');
     $oxum->enqueue_script('vendors.min.js', 'js/');
-	$oxum->enqueue_style('css/main.css');
-	$oxum->enqueue_script('main.min.js', 'js/');
+    $oxum->enqueue_style('css/main.css');
+    $oxum->enqueue_script('main.min.js', 'js/');
 }
 
-function print_processed_html($string)
-{
-    $search  = Array("'");
-    $replace = Array("\'");
+function print_processed_html($string) {
+    $search = array("'");
+    $replace = array("\'");
 
-    $processed_string = str_replace($search, $replace , $string);
-
-    echo $processed_string;
+    return str_replace($search, $replace, $string);
 }
-add_action( 'wp_enqueue_scripts', 'enqueue_scripts');
+
+add_action('wp_enqueue_scripts', 'enqueue_scripts');
