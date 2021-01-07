f6610903efd57d75c01697b6c56880182215b146
julio <julio@paniagua.dev>
2021-01-07 16:20:12 +0100

    Add missing include

--- src/server/front-page.php
+++ src/server/front-page.php
@@ -12,53 +12,7 @@
 	$loop = new WP_Query($args);
 ?>
     <section class="section section-content shadow-lg">
-        <script>
-            var artworks = {
-                works: [
-					<?php
-					$categories = array();
-					/** ARTWORK UI CONFIG **/
-					while($loop->have_posts()) : $loop->the_post();
-					$category = print_processed_html(get_the_category()[0]->name);
-					?>
-                    {
-                        src: '<?php echo htmlspecialchars(get_field('painting')); ?>',
-                        description: '<?php echo htmlspecialchars(get_field('description'));?>',
-                        label: '<?php the_title(); ?>',
-                        category: '<?php echo $category; ?>',
-                        filter: '<?php echo htmlspecialchars(get_field('year')); ?>'
-                    },
-					<?php
-
-					/** SET CATEGORIES **/
-					$categories[$category] = $category;
-
-					/** SET FILTERS **/
-					$filters[get_field('year')] = get_field('year');
-					?>
-					<?php endwhile; ?>
-                ],
-                categories: [
-					<?php
-					foreach($categories as $category) {
-						if(!empty($category) && $category !== '') {
-							echo "'" . $category . "',";
-						}
-					}
-					?>
-                ],
-                filters: [
-					<?php
-					foreach($filters as $filter) {
-						if(!empty($filter) && $filter !== '') {
-							echo "'" . $filter . "',";
-						}
-					}
-					?>
-                ],
-                filterLabel: 'Years ',
-            };
-        </script>
+        <?php include('inc/artowrks-loop.php'); ?>
         <div
             id="artwork-ui"
             class="artwork-ui container"
