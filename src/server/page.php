fd214e3d4ee3f505c01c1daa6caf3904efbb72a8
julio <julio@paniagua.dev>
2021-01-25 22:13:02 +0100

    Remove animation 'cause it broke the behaviour and content take long to be displayed scrolling.

--- src/server/page.php
+++ src/server/page.php
@@ -1,14 +1,14 @@
 <?php
-	get_header();
-	get_template_part('template-parts/page/section', 'header');
+get_header();
+get_template_part('template-parts/page/section', 'header');
 ?>
 <section class="section section-content shadow-lg">
-    <div class="container-md" uk-scrollspy="target: .wp-block-column; cls: opacity; delay: 500">
-		<?php if(have_posts()) : while(have_posts()) :
-			the_post(); ?>
-			<?php the_content(); ?>
-		<?php endwhile; ?>
-		<?php endif; ?>
+    <div class="container-md">
+        <?php if (have_posts()) : while (have_posts()) :
+            the_post(); ?>
+            <?php the_content(); ?>
+        <?php endwhile; ?>
+        <?php endif; ?>
     </div>
 </section>
 <?php get_footer(); ?>
