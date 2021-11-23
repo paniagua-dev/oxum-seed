48662359bc17a2469e0c5d809e658a4f4a64c6c9
Paniagua Julio <julio.paniagua@caisse-des-medecins.ch>
2021-11-23 15:30:51 +0100

    Remove Artworks from the seed project

--- src/server/front-page.php
+++ src/server/front-page.php
@@ -1,24 +1,15 @@
 <?php
-	get_header();
-	get_template_part('template-parts/section', 'header');
+get_header();
+get_template_part('template-parts/section', 'header');
 
-	/** GET POST TYPE ARTWORKS **/
-	$args = array(
-		'post_type' => 'artwork',
-		'post_status' => 'publish',
-		'posts_per_page' => -1,
-		'order' => 'DEC'
-	);
-	$loop = new WP_Query($args);
+/** GET POST TYPE ARTWORKS **/
+$args = array(
+    'post_type' => 'artwork',
+    'post_status' => 'publish',
+    'posts_per_page' => -1,
+    'order' => 'DEC'
+);
+$loop = new WP_Query($args);
 ?>
-<section id="firstSection" class="section section-content shadow-lg">
-    <?php include('inc/artworks-loop.php'); ?>
-        <div
-            id="artwork-ui"
-            class="artwork-ui container"
-            uk-scrollspy="target: .portfolio > .item-in-view; cls: opacity; delay: 500"
-            style="max-width: 1030px; margin: auto;"
-        >
-        </div>
-    </section>
+<!-- CONTENT GOES HERE -->
 <?php get_footer(); ?>
