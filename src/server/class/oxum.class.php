bb091c68780da81e7ca3ad2088cb475cc9d68b67
jup <julio.panaigua@caisse-des-medecins.ch>
2021-12-03 10:44:58 +0100

    Clean project

--- src/server/class/oxum.class.php
+++ src/server/class/oxum.class.php
@@ -1,63 +1,63 @@
 <?php
 
-	/**
-	 * Class helper to handle Wordpress theme
-	 * @Important should be imported in the Wordpress file functions.php
-	 */
+/**
+ * Class helper to handle Wordpress theme
+ * @Important should be imported in the Wordpress file functions.php
+ */
 
-	class WP_Oxum {
-		public $theme_path;
+class WP_Oxum {
+    public $theme_path;
 
-		function __construct() {
-		}
+    function __construct() {
+    }
 
-		/**
-		 * Enable basic Wordpress supports
-		 */
-		public function preset_wordpress() {
-			//Add Featured Images & Post Thumbnails support
-			add_theme_support('post-thumbnails');
+    /**
+     * Enable basic Wordpress supports
+     */
+    public function preset_wordpress() {
+        //Add Featured Images & Post Thumbnails support
+        add_theme_support('post-thumbnails');
 
-			//Add <title> tag support in the header
-			add_theme_support('title-tag');
+        //Add <title> tag support in the header
+        add_theme_support('title-tag');
 
-			//Add Navigation Menu Support
-			add_action('init', array($this, 'register_menu'));
+        //Add Navigation Menu Support
+        add_action('init', array($this, 'register_menu'));
 
-		}
+    }
 
-		private function get_theme_path() {
-			return (isset($this->theme_path)) ? $this->theme_path : get_template_directory_uri() . '/';
-		}
+    private function get_theme_path() {
+        return (isset($this->theme_path)) ? $this->theme_path : get_template_directory_uri() . '/';
+    }
 
-		public function register_menu() {
-			register_nav_menu('Main menu', 'Main navigation menu');
-		}
+    public function register_menu() {
+        register_nav_menu('Main menu', 'Main navigation menu');
+    }
 
-		public function enqueue_style($style_name, $dependencies = [], $version = '1.0.0', $media = 'all') {
-			wp_enqueue_style($style_name, $this->get_theme_path() . $style_name, $dependencies, $version, $media);
-		}
+    public function enqueue_style($style_name, $dependencies = [], $version = '1.0.0', $media = 'all') {
+        wp_enqueue_style($style_name, $this->get_theme_path() . $style_name, $dependencies, $version, $media);
+    }
 
-		public function enqueue_external_style($style_name, $style_url, $dependencies = [], $version = '1.0.0', $media = 'all') {
-			wp_enqueue_style($style_name, $style_url, $dependencies, $version, $media);
-		}
+    public function enqueue_external_style($style_name, $style_url, $dependencies = [], $version = '1.0.0', $media = 'all') {
+        wp_enqueue_style($style_name, $style_url, $dependencies, $version, $media);
+    }
 
-		public function enqueue_script($script_name, $path = '', $dependencies = [], $version = '1.0.0', $in_footer = true) {
-			wp_enqueue_script($script_name, $this->get_theme_path() . $path . $script_name, $dependencies, $version, $in_footer);
-		}
+    public function enqueue_script($script_name, $path = '', $dependencies = [], $version = '1.0.0', $in_footer = true) {
+        wp_enqueue_script($script_name, $this->get_theme_path() . $path . $script_name, $dependencies, $version, $in_footer);
+    }
 
-		public function enqueue_external_script($script_name, $script_url, $dependencies = [], $version = '1.0.0', $in_footer = true) {
-			wp_enqueue_script($script_name, $script_url, $dependencies, $version, $in_footer);
-		}
+    public function enqueue_external_script($script_name, $script_url, $dependencies = [], $version = '1.0.0', $in_footer = true) {
+        wp_enqueue_script($script_name, $script_url, $dependencies, $version, $in_footer);
+    }
 
-		/**
-		 * Remove JQuery from the front-end part of Wordpress
-		 * @import should be run in the hook 'init'
-		 */
-		public function no_more_jquery() {
-			if(!is_admin()) {
-				wp_deregister_script('jquery');
-				wp_register_script('jquery', false);
-			}
-		}
-	}
+    /**
+     * Remove JQuery from the front-end side
+     * @import should be run in the hook 'init'
+     */
+    public function no_more_jquery() {
+        if (!is_admin()) {
+            wp_deregister_script('jquery');
+            wp_register_script('jquery', false);
+        }
+    }
+}
